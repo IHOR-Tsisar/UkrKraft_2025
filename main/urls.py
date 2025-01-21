6cfc45c95c1102366dcddd7f65dec6d228efd3e0
DELL <ilz2008@ukr.net>
2025-01-21 17:56:59 +0400

    app goods 21.01.2025

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+UkrKraft_2025_name='main'
+from django.urls import path
+
+from main import views
+
+app_name = 'main'  # Визначення простору імен
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('delivery_payment/', views.delivery_payment, name='delivery_payment'),
+    path('contacts/', views.contacts, name='contacts'),
+]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
-# Create your views here.
+
 def index(request):
-    return render(request,'main/index.html')
+    context= {
+        'title': 'Kraft - головна',
+        'content':'Магазин крафтових сирів і напоїв'
+    }
+    return render(request,'main/index.html',context )
 
 def about(request):
-    return HttpResponse("About Page")
+    context = {
+        'title': 'Kraft - Про нас',
+        'content': "Про нас",
+        'text_on_page': 'Чому наша продукція така добра.'
+    }
+    return render(request, 'main/about.html', context)
+
+
+def delivery_payment(request):
+    context = {
+        'title': 'Оплата і доставка',
+        'content': "Оплата і доставка",
+        'text_on_page': 'Про оплату і доставку.'
+    }
+    return render(request, 'main/delivery_payment.html', context)
+
+def contacts(request):
+    context = {
+        'title': 'Контакти',
+        'content': "Наші контакти",
+        'text_on_page': 'Контактна інформація.'
+    }
+    return render(request, 'main/contacts.html', context)
