e66fe7bfd251bf11af80c334b4c3d8aa6d9b3a41
DELL <ilz2008@ukr.net>
2025-01-26 19:07:12 +0400

    categories & products 26/01/25

--- goods/models.py
+++ goods/models.py
@@ -1,2 +1,33 @@
 from django.db import models
 
+class Categories(models.Model):
+    name = models.CharField(max_length=255,unique=True, verbose_name="Названиe")
+    slag = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name="URL")
+    isvisible = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Названиe")
+    slag = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name="URL")
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.DecimalField(default=0.00, max_digits=6, decimal_places=2, verbose_name="Цена")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name="Изображение")
+    discount = models.DecimalField(default=0.00, max_digits=6, decimal_places=2, verbose_name="Скидка")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    isvisible = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,14 @@
 UkrKraft_2025_name='main'
 from django.urls import path
-
+from django.contrib import admin
 from main import views
 
 app_name = 'main'  # Визначення простору імен
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('delivery_payment/', views.delivery_payment, name='delivery_payment'),
-    path('contacts/', views.contacts, name='contacts'),
+
+     path('about/', views.about, name='about'),
+     path('delivery_payment/', views.delivery_payment, name='delivery_payment'),
+     path('contacts/', views.contacts, name='contacts'),
 ]
