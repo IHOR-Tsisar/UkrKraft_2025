4efa88baae087bd1495c5c127d79781a7900dd80
IHOR-Tsisar <ilz2008@ukr.net>
2025-02-06 17:20:13 +0400

    "pagination 06/02/25"

--- goods/models.py
+++ goods/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-
-
-
 class Categories(models.Model):
     name = models.CharField(max_length=255,unique=True, verbose_name="Названиe")
     slug = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name="URL")
@@ -12,6 +9,7 @@ class Categories(models.Model):
         db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
+        ordering=('id',)
 
     def __str__(self):
         return self.name
